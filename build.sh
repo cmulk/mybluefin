0e2a610805cad314801c414c550d9dc16c35e19b
cmulk <17730096+cmulk@users.noreply.github.com>
2025-01-01 19:41:25 +0000

    try adding displaylink

--- build.sh
+++ build.sh
@@ -13,11 +13,17 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen
+rpm-ostree install micro wget
+
+wget https://github.com/displaylink-rpm/displaylink-rpm/releases/download/v6.1.0-2/fedora-41-displaylink-1.14.7-4.github_evdi.x86_64.rpm
+rpm-ostree install ./fedora-41-displaylink-1.14.7-4.github_evdi.x86_64.rpm
+
+dkms add evdi/1.14.7
+dkms autoinstall
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+#systemctl enable podman.socket
